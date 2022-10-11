670815773aa95c50344f5e5bc09693a63dfba43d
alihmzyv <alihmzyv@gmail.com>
2022-10-12 00:01:12 +0400

    can return a User by email and password

--- src/main/java/dao/services/UserService.java
+++ src/main/java/dao/services/UserService.java
@@ -3,10 +3,21 @@ package dao.services;
 import dao.dao.DAO;
 import entities.User;
 
+import java.sql.PreparedStatement;
+import java.util.Optional;
+
 public class UserService {
     private DAO<User> dao;
 
     public UserService(DAO<User> dao) {
         this.dao = dao;
+    }
+
+    public Optional<User> get(User user) {
+        return dao.get(user);
+    }
+
+    public Optional<User> get(String emailAddress, String password) {
+        return dao.get(new User(emailAddress, password));
     }
 }

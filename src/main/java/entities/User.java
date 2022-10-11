0f7a425b9a56b74a8ab32ea32ebb4074ffdbc55b
alihmzyv <alihmzyv@gmail.com>
2022-10-11 21:36:21 +0400

    added name, surname fields to User

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,11 +2,15 @@ package entities;
 
 public class User implements Identifiable {
     private long id;
+    private String name;
+    private String surname;
     private String username;
     private String profilePicLink;
 
-    public User(long id, String username, String profilePicLink) {
+    public User(long id, String name, String surname, String username, String profilePicLink) {
         this.id = id;
+        this.name = name;
+        this.surname = surname;
         this.username = username;
         this.profilePicLink = profilePicLink;
     }

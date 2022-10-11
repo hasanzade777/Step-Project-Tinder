70fa7135814dee9dce3ba0c8d50754d0e23d99b7
alihmzyv <alihmzyv@gmail.com>
2022-10-11 21:49:43 +0400

    added password field to User.java

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -5,13 +5,15 @@ public class User implements Identifiable {
     private String name;
     private String surname;
     private String username;
+    private String password;
     private String profilePicLink;
 
-    public User(long id, String name, String surname, String username, String profilePicLink) {
+    public User(long id, String name, String surname, String username, String password, String profilePicLink) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.username = username;
+        this.password = password;
         this.profilePicLink = profilePicLink;
     }
 

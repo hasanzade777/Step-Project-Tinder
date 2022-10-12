105d9047395c713f772c427f61150cfa63bfed7b
alihmzyv <alihmzyv@gmail.com>
2022-10-12 13:49:10 +0400

    /like-page servlet initialization

---
+++ src/main/java/servlets/LikePageServlet.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+// http://localhost:8080/like-page
+public class LikePageServlet extends HttpServlet {
+    private Template templ;
+
+    @Override
+    public void init() throws ServletException {
+        Configuration conf = new Configuration(Configuration.VERSION_2_3_28);
+        conf.setDefaultEncoding(String.valueOf(StandardCharsets.UTF_8));
+        try {
+            conf.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            templ = conf.getTemplate("like-page");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        throw new RuntimeException();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        throw new RuntimeException();
+    }
+}

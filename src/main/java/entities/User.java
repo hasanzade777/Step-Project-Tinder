7aaee0efe6d695627e66959c578fdfa5017ad8fa
alihmzyv <alihmzyv@gmail.com>
2022-10-12 13:46:27 +0400

    added job, last login date and time fields

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,24 +2,32 @@ package entities;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class User implements Identifiable {
     private long id;
     private String name;
     private String surname;
+    private String job;
     private String emailAddress;
     private String username;
     private String password;
     private String profilePicLink;
+    private LocalDateTime lastLoginDateTime;
+    private LocalDate lastLoginDate;
 
-    public User(long id, String name, String surname, String emailAddress, String username, String password, String profilePicLink) {
+    public User(long id, String name, String surname, String job, String emailAddress, String username, String password, String profilePicLink, LocalDateTime lastLoginDateTime) {
         this.id = id;
         this.name = name;
         this.surname = surname;
+        this.job = job;
         this.emailAddress = emailAddress;
         this.username = username;
         this.password = password;
         this.profilePicLink = profilePicLink;
+        this.lastLoginDateTime = lastLoginDateTime;
+        this.lastLoginDate = lastLoginDateTime.toLocalDate();
     }
 
     public User(String emailAddress, String password) {
@@ -40,19 +48,31 @@ public class User implements Identifiable {
         return password;
     }
 
-    public static User of(long id, String name, String surname, String emailAddress, String username, String password, String profilePicLink) {
-        return new User(id, name, surname, emailAddress, username, password, profilePicLink);
+    public LocalDate getLastLoginDate() {
+        return lastLoginDate;
     }
+
+    public String getProfilePicLink() {
+        return profilePicLink;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
 
     public static User getFromResultSet(ResultSet rs) {
         try {
-            return User.of(rs.getLong("id"),
+            return new User(rs.getLong("id"),
                     rs.getString("name"),
                     rs.getString("surname"),
+                    rs.getString("job"),
                     rs.getString("email_address"),
                     rs.getString("username"),
                     rs.getString("password"),
-                    rs.getString("profile_pic_link"));
+                    rs.getString("profile_pic_link"),
+                    rs.getTimestamp("last_login_date_time").toLocalDateTime());
         }
         catch (SQLException e) {
             throw new RuntimeException(e);

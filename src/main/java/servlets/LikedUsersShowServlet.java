7fa7e312892969acba990615d675078b4dcb5f07
alihmzyv <alihmzyv@gmail.com>
2022-10-12 13:51:49 +0400

    class files etc

---
+++ src/main/java/servlets/LikedUsersShowServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class LikedUsersShowServlet extends HttpServlet {
+
+    private Template templ;
+
+    @Override
+    public void init() throws ServletException {
+        Configuration conf = new Configuration(Configuration.VERSION_2_3_28);
+        conf.setDefaultEncoding(String.valueOf(StandardCharsets.UTF_8));
+        try {
+            conf.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            templ = conf.getTemplate("people-list");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        throw new RuntimeException();
+    }
+}

4a2ee673e6d9c51350e071363480f48ce009de29
alihmzyv <alihmzyv@gmail.com>
2022-10-12 13:51:33 +0400

    login filter added

---
+++ src/main/java/filters/LoginFilter.java
@@ -0,0 +1,33 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        boolean hasLoggedIn = Arrays.stream(req.getCookies()).anyMatch(cookie -> cookie.getName().equals("c_user"));
+        if (hasLoggedIn) {
+            chain.doFilter(request, response);
+        }
+        else {
+            HttpServletResponse resp = (HttpServletResponse) request;
+            resp.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

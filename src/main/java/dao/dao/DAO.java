2c31de094f0cec6cdae4380de396f58c23b61934
alihmzyv <alihmzyv@gmail.com>
2022-10-22 18:03:09 +0400

    added save, getConn methods to Dao

--- src/main/java/dao/dao/DAO.java
+++ src/main/java/dao/dao/DAO.java
@@ -1,13 +1,17 @@
 package dao.dao;
 
 import entities.Identifiable;
+
+import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
 
 public interface DAO<A extends Identifiable> {
+    void save(A obj);
     Optional<A> get(Long id);
 
     Optional<A> get(A obj);
 
     List<A> getAll();
+    Connection getConn();
 }

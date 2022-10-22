5e16af32d91684c2ec160495db46b795522988e5
alihmzyv <alihmzyv@gmail.com>
2022-10-23 02:00:10 +0400

    deleted empty lines

--- src/main/java/services/MessageService.java
+++ src/main/java/services/MessageService.java
@@ -5,8 +5,6 @@ import entities.Message;
 import java.util.List;
 
 public interface MessageService {
-
     void saveMessage(Message message);
-
     List<Message> getAllMessagesBetween(Long user1, Long user2);
 }

b1f7de1c1bb80c62585ca577485aba98db278033
alihmzyv <alihmzyv@gmail.com>
2022-10-22 19:52:35 +0400

    deleted empty lines

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -2,17 +2,15 @@ package services;
 
 import entities.User;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    Optional<User> get(User user);
-
-    Optional<User> get(String emailAddress, String password);
-
-    List<User> getAll();
-
+    Optional<User> getUser(Long id);
+    Optional<User> getUser(User user);
+    Optional<User> getUser(String emailAddress, String password);
+    List<User> getAllUsers();
     void updateLastLoginDateTime(Long id);
-
-    Optional<User> getUserByID(Long id);
+    Connection getConn();
 }

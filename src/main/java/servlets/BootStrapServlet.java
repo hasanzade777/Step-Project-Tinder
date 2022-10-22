ff468023bb378f20cb5275638b61874b07b2c1b4
alihmzyv <alihmzyv@gmail.com>
2022-10-22 19:42:47 +0400

    substring

--- src/main/java/servlets/BootStrapServlet.java
+++ src/main/java/servlets/BootStrapServlet.java
@@ -15,7 +15,7 @@ public class BootStrapServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String fileName = Objects.requireNonNull(getClass().getClassLoader().getResource("templates/css/bootstrap.min.css"))
-                .getFile();
+                .getFile().substring(1);
         try (PrintWriter pw = resp.getWriter()) {
             Files.readAllLines(Path.of(fileName)).forEach(pw::println);
         }
